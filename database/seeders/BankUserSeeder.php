49dc0230fd81adbaf2795d5344cbfd10444a4070
HoeunRaksa <hoeunraksa12@gmail.com>
2026-02-13 16:21:38 +0700

    feat: add BankUserSeeder for user account

---
+++ database/seeders/BankUserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\BankAccount;
+use Illuminate\Support\Facades\Hash;
+
+class BankUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        BankAccount::updateOrCreate(
+            ['account_number' => '1122334455'],
+            [
+                'first_name' => 'Hoeun',
+                'last_name' => 'Raksa',
+                'password' => Hash::make('password123'),
+                'balance' => 50000.00,
+                'currency' => 'USD',
+                'is_active' => true,
+            ]
+        );
+    }
+}
