a6a92024738786b27561e72d255626a9a625cdf9
HoeunRaksa <hoeunraksa12@gmail.com>
2026-02-13 16:43:05 +0700

    chore: remove temporary database scripts

--- create_acc.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO bank_accounts (account_number, first_name, last_name, password, balance, is_active, created_at, updated_at) 
-VALUES ('1122334455', 'Hoeun', 'Raksa', '$2y$12$1HK0K.oebrxMJg.gczL6RuCAVlxWkwJpQfUTBPdQ9N33MIeuAOyVC', 50000.00, 1, NOW(), NOW())
-ON DUPLICATE KEY UPDATE first_name='Hoeun', balance=50000.00;
