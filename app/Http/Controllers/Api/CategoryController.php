3b2313cbe2ac81c0b4cc8a0f54833b454c890ab1
HoeunRaksa <197785578+HoeunRaksa@users.noreply.github.com>
2026-03-21 10:14:43 +0000

    style: fix code style with Laravel Pint

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use OpenApi\Attributes as OA;
 
